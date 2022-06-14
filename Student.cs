6e998118ed746a2f3de4809b442533376a04472a
mangal813 <82094784+mangal813@users.noreply.github.com>
2022-06-14 17:45:34 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp3
+{
+    internal class Student
+    {
+        private int v1;
+        private string v2;
+        private object female;
+        private object iT;
+
+        public Student(int v1, string v2, object female, object iT)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.female = female;
+            this.iT = iT;
+        }
+    }
+}
